1b54041824392c33355b25d72b35daefd14e7387
Egyember <gyengezsombor2005@gmail.com>
2025-09-07 22:30:13 +0200

    added tag shortcuts

---
+++ lua/myconfig/tags.lua
@@ -0,0 +1,7 @@
+vim.keymap.set('n', '<leader>tt', function ()
+	vim.cmd.tag(vim.fn.expand("<cword>"))
+end, {})
+vim.keymap.set('n', '<leader>tr', function ()
+	vim.cmd.helptags("./")
+	print("regenerated tags")
+end, {})
